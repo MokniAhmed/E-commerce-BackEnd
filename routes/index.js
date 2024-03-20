a535d21f729e9beb872c77a84793b602f44a94cb
MokniAhmed <mokni.ahmed.am@gmail.com>
2024-03-21 00:57:12 +0100

    add brand crud

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const categoryRoute = require('./categoryRoute');
 const userRoute = require('./userRoute');
 const authRoute = require('./authRoute');
 const subCategoryRoute = require("./subCategoryRoute");
+const brandRoute = require("./brandRoute");
 
 const mountRoutes = (app) => {
   app.use("/api/v1/auth", authRoute);
   app.use("/api/v1/users", userRoute);
   app.use("/api/v1/categories", categoryRoute);
   app.use("/api/v1/subCategory", subCategoryRoute);
+  app.use("/api/v1/brand", brandRoute);
 };
 
 module.exports = mountRoutes;
